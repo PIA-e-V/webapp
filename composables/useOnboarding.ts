154ded498332fedef80a8b88c4e72c52c5adc2c9
Friedrich Waack <friedrich.waack@programado.de>
2021-09-25 17:56:35 +0200

    implement onboarding for new app

--- composables/useOnboarding.ts
+++ composables/useOnboarding.ts
@@ -1,29 +1,29 @@
 import { onMounted, useRoute, useRouter, watch } from '@nuxtjs/composition-api'
 
-const exceptions = [
-  'login',
-  'registration',
-  'verify-email',
-  'onboarding'
-]
+const exceptions = ['login', 'registration', 'verify-email', 'onboarding']
 export default function () {
   const route = useRoute()
   const router = useRouter()
 
-  function checkIfOnboardingIsNeeded () {
+  function checkIfOnboardingIsNeeded() {
     if (localStorage.getItem('onboarding.initial') !== '1') {
       router!.push('/onboarding/initial')
+    } else if (localStorage.getItem('onboarding.new-version') !== '1') {
+      router!.push('/onboarding/new-version')
     } else if (localStorage.getItem('onboarding.login') !== '1') {
       router!.push('/onboarding/login')
     }
   }
 
-  watch(() => route.value.path, () => {
-    checkIfOnboardingIsNeeded()
-  })
+  watch(
+    () => route.value.path,
+    () => {
+      checkIfOnboardingIsNeeded()
+    }
+  )
 
   onMounted(() => {
-    if (exceptions.some(e => route.value.path.includes(e))) {
+    if (exceptions.some((e) => route.value.path.includes(e))) {
       return
     }
 
---
+++ pages/onboarding/new-version/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="h-full">
+    <img src="/onboarding/new-version.png" class="mx-auto block mt-5" style="height: 30vh" alt="Update" />
+
+    <div class="mt-5 px-4">
+      <p>
+        Am 27. September werden wir ein neues Update veröffentlichen. Du wirst dann die wichtigsten politischen
+        Nachrichten, Gesetze und Petitionen verfolgen und darüber abstimmen können.
+      </p>
+    </div>
+
+    <div class="btn-container">
+      <Button class="forward-btn" background="white" color="#3A4090FF" @click="$router.push('/')">
+        Weiter geht's!
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, useRouter } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  layout: 'onboarding',
+  setup() {
+    const router = useRouter()
+
+    onMounted(() => {
+      localStorage.setItem('onboarding.new-version', '1')
+    })
+
+    return {
+      async proceedWithoutLogin() {
+        router!.push('/')
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+* {
+  color: white;
+
+  @apply text-center;
+}
+
+h1 {
+  font-size: 15pt;
+
+  @apply font-bold;
+}
+
+h2 {
+  color: #ffdf57;
+  font-size: 15pt;
+
+  @apply font-bold;
+}
+
+.btn-container {
+  bottom: 20px;
+  @apply absolute w-full;
+
+  .forward-btn {
+    @apply block mx-auto shadow;
+  }
+}
+</style>
